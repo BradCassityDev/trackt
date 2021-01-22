602e971f507bf2fac86d0f808fea86a29e6f0142
Brad Cassity <bradley.cassity@gmail.com>
2021-01-22 08:21:06 -0700

    Login form created

---
+++ client/src/pages/Signup.js
@@ -0,0 +1,82 @@
+import React, { useState } from 'react';
+// import {  } from '../utils/mutations';
+import { Link } from 'react-router-dom';
+
+import Auth from '../utils/auth';
+
+const Signup = () => {
+
+    // Form state
+    const [formState, setFormState] = useState({ email: '', password: '' });
+
+    // Signup mutation
+    // const [Signup, { error }] = useMutation();
+
+    // Handle form state if change is detected
+    const formChange = event => {
+        // Deconstruct the name and value from the target field
+        const { name, value } = event.target;
+
+        // Set the form's state with the new values
+        setFormState({
+            ...formState,
+            [name]: value
+        });
+    };
+
+    // Handle Signup form submit
+    const handleFormSubmit = event => {
+        event.preventDefault();
+    };
+
+    return (
+        <div className="card login-signup-card shadow-sm">
+            <div className="card-body">
+                <h4>Login</h4>
+                <hr></hr>
+                {true && <div className="alert alert-dange">Login failed. Incorrect email or password.</div>}
+                <form onSubmit={handleFormSubmit} className="login-form">
+                    <div className="row">
+                        <div className="col-12 mb-3">
+                            <label className="form-label">Email</label>
+                            <input
+                                className='form-control'
+                                placeholder='Email'
+                                name='email'
+                                type='email'
+                                id='email'
+                                value={formState.email}
+                                onChange={formChange}
+                            />
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col-12 mb-3">
+                            <label className="form-label">Password</label>
+                            <input
+                                className='form-control'
+                                placeholder='Password'
+                                name='password'
+                                type='password'
+                                id='emapasswordil'
+                                value={formState.password}
+                                onChange={formChange}
+                            />
+                        </div>
+                    </div>
+                    <button className='btn w-100 btn-default' type='submit'>
+                        Submit
+                    </button>
+                </form>
+
+                <div className="login-signup-toggle-text">
+                    <p>Don't have an account?</p>
+                    <p><Link to="/login">LOGIN</Link> instead</p>
+                </div>
+                
+            </div>
+        </div>
+    );
+};
+
+export default Signup;
