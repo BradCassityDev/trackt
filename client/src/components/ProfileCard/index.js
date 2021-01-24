599a0ef32aa2bb816b4ce305a29ac216b5c3665c
Christian <christian.dehek@gmail.com>
2021-01-24 15:09:29 -0700

    integrated cloudinary widget

--- client/src/components/ProfileCard/index.js
+++ client/src/components/ProfileCard/index.js
@@ -1,62 +1,114 @@
-import React from 'react';
-import Auth from '../../utils/auth';
-import { useQuery, useMutation } from '@apollo/react-hooks';
-import image from '../../images/placeholder-profile-pic.png';
+import React, { useState } from "react";
+import Auth from "../../utils/auth";
+import { useQuery, useMutation } from "@apollo/react-hooks";
+import image from "../../images/placeholder-profile-pic.png";
+import { UPDATE_USER } from "../../utils/mutations";
 
 const ProfileCard = ({ user }) => {
+    
+  const [updateUser, { error }] = useMutation(UPDATE_USER);
 
-    // Handle user edit form submit
-    const handleEditUserSubmit = async event => {
-        event.preventDefault();
-    };
-
-
-    const renderCardDetails = () => {
-        if (Auth.getProfile().data.username !== user.username) {
-            return (
-                <div>
-                    Name: {user.firstName} {user.lastName}
-                    <button type="button" className="btn btn-default">Add Friend</button>
-                </div>
-            );
-        } else {
-            return (
-                <form
-                    className="edit-profile-form"
-                    onSubmit={handleEditUserSubmit}
-                >
-                    <div>
-                        <label>First Name:</label>
-                        <input type="text" name="firstname" id="firstname" className="form-control"></input>
-                    </div>
-                    <div>
-                        <label>Last Name:</label>
-                        <input type="text" name="lastname" id="lastname" className="form-control"></input>
-                    </div>
-                    <div>
-                        <label>Email:</label>
-                        <input type="text" name="email" id="email" className="form-control"></input>
-                    </div>
-                    <div>
-                        <button type="submit" className="btn btn-default">Save Changes</button>
-                    </div>
-                </form>
-            );
+  function showUploadWidget() {
+    window.cloudinary.openUploadWidget(
+      {
+        cloudName: "trackt",
+        uploadPreset: "trackt",
+        cropping: true,
+      },
+      (error, result) => {
+        if (!error && result && result.event === "success") {
+          const pfp = result.info.secure_url;
+          console.log(pfp);
+          user.profilePhoto = pfp
+          updateUser({
+              variables: {
+                  profilePhoto: user.profilePhoto,
+                  _id: user._id
+              }
+          })
         }
-    }
-
-    return (
-        <div className="content-wrapper">
-            <div className="card text-center profile-card">
-                <div className="card-body">
-                    <img src={user.profilePhoto ? user.profilePhoto : image} alt="profile pic" className="rounded-circle profile-image"/>
-                    <h5 className="card-title">{user.username}</h5>
-                    <hr></hr>
-                    {renderCardDetails()}
-                </div>
-            </div>
-        </div>
+      }
     );
+  }
+  console.log(user.profilePhoto);
+
+
+  // Handle user edit form submit
+  const handleEditUserSubmit = async (event) => {
+    event.preventDefault();
+  };
+
+  const renderCardDetails = () => {
+    if (Auth.getProfile().data.username !== user.username) {
+      return (
+        <div>
+          Name: {user.firstName} {user.lastName}
+          <button type="button" className="btn btn-default">
+            Add Friend
+          </button>
+        </div>
+      );
+    } else {
+      return (
+        <form className="edit-profile-form" onSubmit={handleEditUserSubmit}>
+          <div>
+            <label>First Name:</label>
+            <input
+              type="text"
+              name="firstname"
+              id="firstname"
+              className="form-control"
+            ></input>
+          </div>
+          <div>
+            <label>Last Name:</label>
+            <input
+              type="text"
+              name="lastname"
+              id="lastname"
+              className="form-control"
+            ></input>
+          </div>
+          <div>
+            <label>Email:</label>
+            <input
+              type="text"
+              name="email"
+              id="email"
+              className="form-control"
+            ></input>
+          </div>
+          <div>
+            <button className="btn btn-default" onClick={showUploadWidget}>
+              Change Profile Picture
+            </button>
+          </div>
+          <div>
+            <button type="submit" className="btn btn-default">
+              Save Changes
+            </button>
+          </div>
+        </form>
+      );
+    }
+  };
+
+  return (
+    <div className="content-wrapper">
+      <div className="card text-center profile-card">
+        <div className="card-body">
+          <img
+            src={user.profilePhoto ? user.profilePhoto : image}
+            alt="profile pic"
+            className="rounded-circle profile-image"
+          />
+          <h5 className="card-title">{user.username}</h5>
+          <hr></hr>
+          {renderCardDetails()}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ProfileCard;
--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -29,7 +29,7 @@ export const ADD_USER = gql`
   }
 `;
 export const UPDATE_USER = gql`
-  mutation updateUser($username: String!, $displayName: String, $email: String!, $firstName: String $lastName: String, $password: String!, $profilePhoto: String) {
+  mutation updateUser($username: String, $displayName: String, $email: String, $firstName: String $lastName: String, $password: String, $profilePhoto: String) {
     updateUser(username: $username, displayName: $displayName, email: $email, firstName: $firstName, lastName: $lastName, password: $password, profilePhoto: $profilePhoto) {
     _id
     username
