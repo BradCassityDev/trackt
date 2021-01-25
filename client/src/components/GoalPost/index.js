47c35ef67cde7cf9eaeaec76b24c5c1dc728a83b
Brad Cassity <bradley.cassity@gmail.com>
2021-01-24 21:25:03 -0700

    Added Failed State

--- client/src/components/GoalPost/index.js
+++ client/src/components/GoalPost/index.js
@@ -1,10 +1,9 @@
 import React, {useState} from 'react';
 import Auth from '../../utils/auth';
 import image from '../../images/placeholder-profile-pic.png';
-
+import failStamp from '../../images/Fail.png';
 import CommentList from '../CommentList';
 import PostHeader from '../PostHeader';
-
 import Collapse from "react-bootstrap/Collapse";
 import Container from "react-bootstrap/Container";
 import Button from "react-bootstrap/Button";
@@ -12,12 +11,19 @@ import Button from "react-bootstrap/Button";
 const GoalPost = ({ goal, username, profilePhoto }) => {
     const [open, setOpen] = useState(false);
 
+    console.log(goal.goalStatus);
+
     return (
         <div className="card goal-post-card shadow-sm">
             <div className="card-header">
                     <PostHeader username={username} createdAt={goal.createdAt} profilePhoto={profilePhoto ? profilePhoto : image} />
             </div>
-            <div className="card-body">
+            <div className="card-body goal-post-card-body">
+                {goal.goalStatus === "Failed" && 
+                    <div className="fail-stamp-container">
+                        <img src={failStamp} alt="Fail Stamp" className="fail-stamp" />
+                    </div>
+                }
                 <h4>{goal.goalTitle}</h4>
                 <p>Category: {goal.goalCategory}</p>
                 <p>Status: {goal.goalStatus}</p>
@@ -38,7 +44,6 @@ const GoalPost = ({ goal, username, profilePhoto }) => {
                     <CommentList comments={goal.comments} goalId={goal._id} username={username} profilePhoto={profilePhoto} />
                     </div>
                 </Collapse>
-                
             </div>
         </div>
     );
